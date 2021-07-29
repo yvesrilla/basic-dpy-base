7f05f892fab8fd66cfaf520d8cb8ec1d90ca100a
mich <76213511+yvesrilla@users.noreply.github.com>
2021-07-29 19:28:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import discord
 from discord.ext import commands
 
-client = new
+client = commands.Bot(command_prefix=".")
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print("i am online and ready!")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("pong!")
+
+client.run("ODY5NzE1ODU4ODUzODgzOTQ1.YQCQAw.mnwe3lCLs86H4mCmFTh5jmzeKRQ")
